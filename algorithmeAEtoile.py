ec63c52a6a60bd5789953f7855e9486e22f5a450
NoixDeXydre <noixdexydre@mailfence.com>
2025-01-28 08:43:52 +0100

    Construction préliminaire de l'algorithme A*

--- algorithmeAEtoile.py
+++ algorithmeAEtoile.py
@@ -1,4 +1,36 @@
+# 
+# algorithmeAEtoile.py                                         28 janvier 2025
+# IUT de Rodez, pas de droits réservés
+#
+
 class AlgorithmeAEtoile:
-    def __init__():
-        # TODO code
-        # ex : self.heuristique = heuristique
+    
+    #
+    # Construit l'instance d'un algorithme de parcours de graphes.
+    # plateau : une instance d'un plateau généré.
+    # calculHeuristique : une méthode avec en entrée un tuple du point 
+    #                     et un plateau. 
+    #                     Permet de calculer l'heuristique d'un point donné.
+    #
+    def __init__(plateau: Plateau, calculHeuristique: lambda):
+        
+        # TODO algorithme de base
+        
+        #
+        # Méthode qui permet de calculer l'heuristique des points voisins
+        # avec en entrée une liste de tuples des prochains points
+        # et le plateau.
+        #
+        self.calculHeuristique = calculHeuristique
+        
+        # Liste de tuples des points encore en analyse.
+        self.listeOuverte = []
+        
+        # Liste de tuples des points à ignorer.
+        self.listeFermee = []
+        
+        # Plateau sélectionné
+        self.plateau = plateau
+        
+    # TODO méthode pour effectuer la prochaine action de l'algorithme
+    # ex : prochainPoint()
