e17df3dea23ff169d5ed3ad8c4d2e513387f82d2
ADRIEN ASTIER <adrien.astier@iut.local>
2025-01-28 08:33:18 +0100

    Création squelette Plateau

--- plateau.py
+++ plateau.py
@@ -1,4 +1,20 @@
 class Plateau:
-    def __init__():
+
+    lignesPlateau = []
+
+    def __init__(lignes):
         # TODO code
         # ex : self.plateau = plateau
+
+    def __init__(largeur, longueur) :
+        # TODO code
+        # ex self.plateau = plateau
+
+    def getCase(i, j) :
+        # TODO Code
+
+    def setCase(i, j, nouvelleValeur) :
+        # TODO Code
+
+    def estValide() :
+        # TODO Code
