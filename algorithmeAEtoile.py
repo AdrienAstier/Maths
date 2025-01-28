6d35c3854e366a2dc37ae87f065564be6b626c95
NOE ARCIER <noe.arcier@iut.local>
2025-01-28 15:45:00 +0100

    def points algo

--- algorithmeAEtoile.py
+++ algorithmeAEtoile.py
@@ -1,43 +1,27 @@
-# 
 # algorithmeAEtoile.py                                         28 janvier 2025
 # IUT de Rodez, pas de droits réservés
-#
 
 import plateau
 
-#
 # Implémente un algorithme afin de trouver le plus court chemin 
 # dans n'importe quels graphes.
 #
 # Dans notre cas, il se base sur une heuristique ainsi qu'un plateau
 # avec des obstacles, un point de départ et d'arrivé.
-#
-#
 class AlgorithmeAEtoile:
 
-    #
     # Construit une instance de l'algorithme de parcours.
     # plateau : une instance d'un plateau généré.
-    # calculHeuristique : une méthode avec en entrée un tuple du point 
+    # calculHeuristique : une méthode avec en entrée un tuple du point actuel
     #                     et un plateau. 
     #                     Permet de calculer l'heuristique d'un point donné.
-    #
     def __init__(plateau: Plateau, calculHeuristique: lambda):
 
-        #
         # Méthode qui permet de calculer l'heuristique des points voisins
-        # avec en entrée une liste de tuples des prochains points
-        # et le plateau.
-        #
+        # avec en entrée le plateau , le point actuel et le point d'arrivée.
         self.calculHeuristique = calculHeuristique
         
-        # Change d'état si le chemin est trouvé au fil des itérations.
-        self.chemin_trouve = False
-        
-        # Liste de tuples des points encore en analyse.
-        self.listeOuverte = []
-        
-        # Liste de tuples des points à ignorer.
+        # Liste de tuples des points à ignorer car déjà traités.
         self.listeFermee = []
 
         # Liste de points du chemin critique.
@@ -45,12 +29,24 @@ class AlgorithmeAEtoile:
         
         # Plateau sélectionné
         self.plateauParcouru = plateau
+
+        self.depart = (0,0)#TODO
+        self.arrive = (0,0)#TODO
+        self.pointActuel = (0,0) #TODO
     
-    #
-    # Calcule la suite du chemin avec les points voisins
-    # et donne le nouveau point parcouru.
-    #
+    def getCheminCritique() :
+        if (len(cheminCritique) == 0) {
+            raise Exception("Plateau non résolu, pas de chemin critique !")
+        }
+        # else
+        return cheminCritique
+
+    # Exécute la prochaine étape de l'algorithme et met à jour le plateau.
     def parcourirProchainPoint():
         
         # TODO méthode
+        pass
+    
+    # Calcul le chemin critique à la fin de l'algorithme et l'ajoute au plateau
+    def calculCheminCritique() :
         pass
