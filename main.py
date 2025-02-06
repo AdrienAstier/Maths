46c32fa296dfc97dd00af4fd395693e878e13eb5
NoixDeXydre <noixdexydre@mailfence.com>
2025-02-06 11:24:56 +0100

    Corrections de quelques bugs et importation correcte du plateau

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from gestionFichier import genererFichier
 from gestionFichier import importationPlateau
 from generationPlateau import genererPlateau
 
+# TODO tourner le menu en boucle même en cas d'erreur
+
 #
 # Lance le menu principal avec toutes
 # les options associées.
 #
-def affichermMenu():
-
-    # TODO tourner le menu en boucle même en cas d'erreur
+def afficherMenuPrincipal():
 
     print("\nBienvenue sur notre application d'optimisation sur le parcours des graphes !\n")
 
@@ -47,28 +47,54 @@ def affichermMenu():
                 chemin_plateau = "N/A"
             
         elif choix == 2:
-            try :
-                nomFichier = input("Veuillez renseigner le nom du fichier à enregistrer : ")
-                longueur = int(input("Entrez un entier pour la longueur du plateau (longueur des lignes) : "))
-                largeur = int(input("Entrez un entier la largeur du plateau (nombre de lignes) : "))
-                taux = float(input("Entrez un nombre flottant entre 0 et 1 pour le taux de murs : "))
-                coins = bool(input("Entrez un booléen, true ou false, pour placer les départs et arrivé dans les coins : "))
-
-                plateauGenere = genererPlateau(longueur, largeur, taux, coins)
-                genererFichier(plateauGenere, nomFichier)
-                print("Le plateau a été généré avec succès dans le dossier 'plateaux_generes'")
-            except ValueError:
-                print("Entrée invalide. Mauvais type.")
+            afficherMenuGeneration()
             
         elif choix == 3:
-            print("Résoudre un plateau")
+            afficherMenuResolution()
+            
         elif choix == 4:
-            print("Comparer Algo")
+            afficherMenuComparaison()
+            
         elif choix == 5:
             print("Vous avez choisi de quitter le logiciel")
         else:
             print("Choix invalide. Veuillez entrer un nombre entre 1 et 5.")
 
+#
+# Affiche le menu de génération de plateaux.
+#
+def afficherMenuGeneration():
+    
+    # TODO éventuellement faire une option pour résoudre après génération le plateau.
+    
+    try:
+        nomFichier = input("Veuillez renseigner le nom du fichier à enregistrer : ")
+        longueur = int(input("Entrez un entier pour la longueur du plateau (longueur des lignes) : "))
+        largeur = int(input("Entrez un entier la largeur du plateau (nombre de lignes) : "))
+        taux = float(input("Entrez un nombre flottant entre 0 et 1 pour le taux de murs : "))
+        coins = bool(input("Entrez un booléen, true ou false, pour placer les départs et arrivé dans les coins : "))
+
+        plateauGenere = genererPlateau(longueur, largeur, taux, coins)
+        genererFichier(plateauGenere, nomFichier)
+        print("Le plateau a été généré avec succès dans le dossier 'plateaux_generes'")
+    
+    except ValueError:
+        print("Entrée invalide. Mauvais type.")
+
+#
+# Affiche le menu de génération de plateaux.
+#
+def afficherMenuResolution():
+    # TODO menu
+    print("Résoudre un plateau")
+   
+#
+# Affiche le menu de comparaison algorithmique.
+#
+def afficherMenuComparaison():
+    # TODO menu
+    print("Comparer algorithmes")
+
 # Lance le menu principal
 if __name__ == "__main__":
-    affichermMenu()
+    afficherMenuPrincipal()
